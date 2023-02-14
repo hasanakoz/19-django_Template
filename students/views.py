8749bd51ab2bba09d662d5b4be75b258dae2e1c0
hasan <hasanakoz1986@gmail.com>
2023-02-14 08:37:19 +0300

    django template exercise

---
+++ students/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+
+    context = {
+        'title' : 'Course',
+        'desc' : 'This is a description',
+        'number' : 3511,
+        'list1' : [1,2,3,4]
+
+    }
+
+    return render(request, 'home.html', context)
+
+    # return HttpResponse('<h1> Hello </h1>')
