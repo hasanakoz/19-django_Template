6575939925e7c564fa140081f579f3ef2f95bc98
hasan <hasanakoz1986@gmail.com>
2023-02-14 22:31:35 +0300

    DTL django forms exercise

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, student_list, student_add, student_update, student_detail, student_delete
+
 
 urlpatterns = [
-    path('', home, name = 'home' )
+    path('', home, name = 'home' ),
+    path('student_list/', student_list, name = 'student_list' ),
+    path('student_add/', student_add, name = 'student_add' ),
+    path('student_update/<int:id>', student_update, name = 'student_update' ),
+    path('student_detail/<int:id>', student_detail, name = 'student_detail' ),
+    path('student_delete/<int:id>', student_delete, name = 'student_delete' ),
 ]
--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import Student
+from .form import StudentForm
 
 # Create your views here.
 
@@ -9,10 +11,84 @@ def home(request):
         'title' : 'Course',
         'desc' : 'This is a description',
         'number' : 3511,
-        'list1' : [1,2,3,4]
+        'list1' : [1,2,3,4],
+        'dict1': {
+            'key1': 'value1',
+            'key2': 'value2'
+        },
+        'dict_list': [
+            {'name': 'zed', 'age': 25},
+            {'name': 'amy', 'age': 21},
+            {'name': 'joe', 'age': 50},
+        ]
+
 
     }
 
     return render(request, 'students/home.html', context)
 
-    # return HttpResponse('<h1> Hello </h1>')
+
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students' : students
+
+    }
+
+    return render(request, 'students/student_list.html', context)
+
+
+
+def student_add(request):
+
+    form = StudentForm()
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'students/student_add.html', context)
+
+
+def student_update(request, id):
+    student = get_object_or_404(Student, id=id)
+    form = StudentForm(instance=student)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES, instance=student)
+
+        
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    context = {
+        'form' : form
+
+    }
+    return render(request, 'students/student_update.html', context)
+
+def student_detail(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    context = {
+        'student' : student
+    }
+    return render (request, 'students/student_detail.html', context)
+
+def student_delete(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student_list')
+
+    context = {
+        'student': student
+    }
+    return render(request, 'students/student_delete.html', context)
